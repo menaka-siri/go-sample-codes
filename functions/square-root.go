40338d30afd12a4a68c1a54fce7753d2c5f1e5bf
Menaka S <menaka442@gmail.com>
2024-03-22 20:00:29 +0530

    improve sqrt function

--- functions/square-root.go
+++ functions/square-root.go
@@ -5,13 +5,22 @@ import (
 	"math"
 )
 
-func Sqrt(x float64) float64 {
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0.0, ErrNegativeSqrt(x)
+	}
 	z := x
 	z2 := x
-	for i:= 0; z2 > 0.001 && i < 10; i++ {
+	for i := 0; z2 > 0.001 && i < 10; i++ {
 		z -= (z*z - x) / (2 * x)
 		z2 = math.Abs(z*z - x)
 		fmt.Printf("i: %d  z: %g Abs(z):%g\n", i, z, z2)
 	}
-	return z
+	return z, nil
 }
